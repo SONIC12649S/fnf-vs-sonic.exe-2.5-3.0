ca533dcddb20d194e57d1e4570955fd4388c466e
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 00:29:24 -0500

    Update DDDDD.lua

--- assets/preload/stages/DDDDD.lua
+++ assets/preload/stages/DDDDD.lua
@@ -1,4 +1,10 @@
 function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
+function onCreate()
     makeLuaSprite('TailsBG','TailsBG',-400,-250)
     scaleObject('TailsBG',1.4,1.4)
     addLuaSprite('TailsBG')
