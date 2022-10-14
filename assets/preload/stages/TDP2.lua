34143189f4c49ba06e7f46d38573955199fe6482
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 00:31:15 -0500

    Update TDP2.lua

--- assets/preload/stages/TDP2.lua
+++ assets/preload/stages/TDP2.lua
@@ -1,3 +1,9 @@
+function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
 local bfVisible = 0
 function onUpdate()
 
