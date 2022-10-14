3937a38815432378fd109a81f20ba8ae159e699d
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 00:23:21 -0500

    Add files via upload

---
+++ assets/preload/scripts/Si lo eliminas crashea.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', "Recreated & Optimized by WALKAY", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 16);
+    addLuaText('watermark');
+end
---
+++ assets/preload/scripts/remove-UI.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+    setProperty('timeBar.visible', false)
+    setProperty('timeBarBG.visible', false)
+    setProperty('timeTxt.visible', false)
+    setProperty('scoreTxt.visible', false)
+end
