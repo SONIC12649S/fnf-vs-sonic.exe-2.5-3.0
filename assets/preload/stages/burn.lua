8e622f749f47fa9a52d347a8bb3fbda6c3138278
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 00:32:08 -0500

    Update burn.lua

--- assets/preload/stages/burn.lua
+++ assets/preload/stages/burn.lua
@@ -1,4 +1,10 @@
 function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
+function onCreate()
 	-- background shit
 	
 	makeAnimatedLuaSprite('cieloburn', 'cieloburn', -600, -720);
